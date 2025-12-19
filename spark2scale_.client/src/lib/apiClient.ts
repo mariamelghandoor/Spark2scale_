caf50269209589e40c74fbeea33a87c637e579bd
Doha <doha.hemdan7@gmail.com>
2025-12-19 19:12:21 +0200

    edited the port

--- spark2scale_.client/src/lib/apiClient.ts
+++ spark2scale_.client/src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://localhost:7155',
+    baseURL: process.env.NEXT_PUBLIC_API_URL2 || 'https://localhost:7155',
     headers: {
         'Content-Type': 'application/json',
     },
