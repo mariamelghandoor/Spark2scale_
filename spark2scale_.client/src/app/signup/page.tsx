1fc0afddf410ae49167ff6420a5b5a1d21b21ed7
sarah.elsayed <s-sarah.elsayed@zewailcity.edu.eg>
2026-01-24 21:03:36 +0200

    commit before pulling from merging branch

--- spark2scale_.client/src/app/signup/page.tsx
+++ spark2scale_.client/src/app/signup/page.tsx
@@ -1,7 +1,6 @@
 ﻿"use client";
 
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -36,7 +35,6 @@ interface ApiResponse {
 
 
 export default function SignupPage() {
-    const router = useRouter();
     const [formData, setFormData] = useState<SignUpFormData>({
         name: "",
         email: "",
